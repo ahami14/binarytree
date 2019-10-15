bb580f7392b9cbd37e7c5f0603c49ffd7cfee7d2
ahami14 <ahnikkahamilton14@gmail.com>
2019-10-14 20:32:13 -0500

    Search and Add methods done

--- BinaryTree/BinaryTree/BinarySearchTree.cs
+++ BinaryTree/BinaryTree/BinarySearchTree.cs
@@ -9,9 +9,58 @@ namespace BinaryTree
     class BinarySearchTree
     {
         //member variables
+        
 
         //constructor
+        Node root = null;
 
         //member methods
+        public void Add(Node root, int data)
+        {
+            if (root == null)
+            {
+                root = new Node(data);
+            }
+        }
+
+        private void AddRec(Node root, int data, Node newNode)
+        {
+            if(root == null)
+            {
+                root = newNode;
+            }
+            if(newNode.data < root.data)
+            {
+                if (root.left == null)
+                {
+                    root.left = newNode;
+                }
+                else
+                {
+                    AddRec(root.left, newNode);
+                }
+            }
+            else
+            {
+                if(root.right == null)
+                {
+                    root.right = newNode;
+                }
+                else
+                {
+                    AddRec(root.right, newNode);
+                }
+            }
+        }
+
+        public void Search(Node root)
+        {
+            if(root == null)
+            {
+                return;
+            }
+            Search(root.right);
+            Search(root.left);
+        }
     }
 }
--- BinaryTree/BinaryTree/Node.cs
+++ BinaryTree/BinaryTree/Node.cs
@@ -8,5 +8,13 @@ namespace BinaryTree
 {
     class Node
     {
+        public int data;
+        public Node right;
+        public Node left;
+
+        public Node(int data)
+        {
+            this.data = data;
+        }
     }
 }
